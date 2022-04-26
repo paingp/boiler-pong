d97872ade6ef5bfe1d59087f9a3850ca704f4ad5
Kenny Stier <kenny@kstr.us>
2022-04-26 19:23:56 -0400

    Remove 'static/

--- background.c
+++ background.c
@@ -1,6 +1,6 @@
 /* GIMP RGB C-Source image dump (background.c) */
 
-static const struct {
+const struct {
   unsigned int 	 width;
   unsigned int 	 height;
   unsigned int 	 bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */ 
